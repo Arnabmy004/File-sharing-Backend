737b706541abd34d2829bb45beeffeb1e0e9546a
Arnab <arnab1090.be22@chitkarauniversity.edu.in>
2025-08-21 20:27:51 +0530

    push

--- src/app.js
+++ src/app.js
@@ -1,25 +1,21 @@
-import express from "express";
-import cors from "cors";
+import express from "express"
+import cors from "cors"
 import "./cron.js";
 
-const app = express();
 
-// ✅ Allow only your deployed frontend
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,  // must be set in .env
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+const app = express()
 
-// Serve temp folder
+app.use(cors(
+  {  origin: process.env.CORS_ORIGIN || "https://send-files-xi.vercel.app"}
+))
 app.use("/temp", express.static("public/temp"));
 
-// Body parsers
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
-// Routes
+export {app}
+
+
 import router from "./routes/file.routes.js";
-app.use("/api/v1/file", router);
 
-export { app };
+app.use("/api/v1/file",router)
